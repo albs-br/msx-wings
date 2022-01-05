e177588ef97373efc222bff631ea0256b5d6ca81
Andre Baptista <albs_br@yahoo.com.br>
2022-01-05 15:23:20 -0300

    v.0.6.0

--- ReadInput.s
+++ ReadInput.s
@@ -2,26 +2,34 @@ ReadInput:
     ; read keyboard
     ld      a, 8                    ; 8th line
     call    BIOS_SNSMAT             ; Read Data Of Specified Line From Keyboard Matrix
+    ld      e, a                    ; save value
 
 
+    bit     0, a                    ; 0th bit (space bar)
+    call    z, .shot
 
-
-    push    af
-        bit     0, a                    ; 0th bit (space bar)
-        call    z, .shot
-    pop     af
-
+    ld      a, e
     bit     4, a                    ; 4th bit (key left)
-    jp      z, .playerLeft
+    call    z, .playerLeft
 
+    ld      a, e
     bit     7, a                    ; 7th bit (key right)
-    jp      z, .playerRight
+    call    z, .playerRight
     
+    ld      a, e
+    bit     5, a                    ; 5th bit (key up)
+    call    z, .playerUp
+
+    ld      a, e
+    bit     6, a                    ; 6th bit (key down)
+    call    z, .playerDown
+
     ret
 
 .playerLeft:
     ld      a, (Player_X)
     sub     3
+    ret     c
     ld      (Player_X), a
 
     ret
@@ -29,9 +37,28 @@ ReadInput:
 .playerRight:
     ld      a, (Player_X)
     add     3
+    cp      240
+    ret     nc
     ld      (Player_X), a
+
+    ret
+
+.playerUp:
+    ld      a, (Player_Y)
+    sub     3
+    ;ret     c
+    ld      (Player_Y), a
+
+    ret
+
+.playerDown:
+    ld      a, (Player_Y)
+    add     3
+    ;ret     c
+    ld      (Player_Y), a
 
     ret
 
 .shot:
     ret
+
