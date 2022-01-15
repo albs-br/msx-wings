8c43f6b5358ceedbbe3aef66593f4c234d1c9f84
Andre Baptista <albs_br@yahoo.com.br>
2022-01-15 19:16:25 -0300

    v.0.16.11

--- LevelData/Level_1.s
+++ LevelData/Level_1.s
@@ -18,7 +18,7 @@ Level_1_Data:
     ;dw  32      db  ENEMY_TYPE_1,           128   dw  EnemyDeltaX_1     dw Enemy_6_Struct, ENEMY_6_SPRCOL_ADDR,      0
     dw  32      db  ENEMY_TYPE_1,           256-16   dw  EnemyDeltaX_1     dw Enemy_6_Struct, ENEMY_6_SPRCOL_ADDR,      0
 
-    dw  40      db  ENEMY_SHOT_TYPE_1,      0     dw  EnemyShotDeltaX_0 dw Enemy_3_Struct, ENEMY_SHOT_0_SPRCOL_ADDR, EnemyShot_0_Struct
+    dw  40      db  ENEMY_SHOT_TYPE_1,      0     dw  EnemyShotDeltaX_22_degrees_left dw Enemy_3_Struct, ENEMY_SHOT_0_SPRCOL_ADDR, EnemyShot_0_Struct
 
 
     dw  150     db  ENEMY_TYPE_1,           64    dw  EnemyDeltaX_2     dw Enemy_0_Struct, ENEMY_0_SPRCOL_ADDR,      0
