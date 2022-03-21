c933b0a3ad13d9f44d30cc724d1016925ca2ed6c
Andre Baptista <albs_br@yahoo.com.br>
2022-03-21 13:30:49 -0300

    v.0.32.4

--- GameLogic/ItemLogic/Item_Logic.s
+++ GameLogic/ItemLogic/Item_Logic.s
@@ -1,2 +1,34 @@
+; Input
+;   HL: addr of item struct
 Item_Logic:
+
+    ; check status before copying to temp vars to save cycles when disabled
+    ld      a, (hl)     ; get Status
+    or      a
+    ret     z           ; if (Status == 0) ret
+    
+    push    hl
+
+        ; Copy item struct to temp item struct
+        ;ld      hl, ?                                          ; source
+        ld      de, Item_Temp_Struct                            ; destiny
+        ld      bc, Item_Temp_Struct.size                       ; size
+        ldir                                                    ; Copy BC bytes from HL to DE
+
+
+        ; TODO: item logic here
+
+
+
+.return:
+        ; increment Frame Counter
+        ld      hl, (Item_Temp_Frame_Counter)
+        inc     hl
+        ld      (Item_Temp_Frame_Counter), hl
+
+        ld      hl, Item_Temp_Struct                        ; source
+    pop     de                                              ; destiny
+    ld      bc, Item_Temp_Struct.size                       ; size
+    ldir                                                    ; Copy BC bytes from HL to DE
+
     ret
