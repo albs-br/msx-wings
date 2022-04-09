898345f91934842b8443733ff0e203fec1090109
Andre Baptista <albs_br@yahoo.com.br>
2022-04-09 19:02:02 -0300

    v.0.38.9

--- GameLogic/GroundTargetLogic/GroundTarget_Logic.s
+++ GameLogic/GroundTargetLogic/GroundTarget_Logic.s
@@ -34,6 +34,29 @@ GroundTarget_Logic:
 
     .isNotScroll:
 
+        ; --------------------------- check collision (ground target x player shots) -------------------------
+
+        ld      a, (GroundTarget_Temp_X)
+        ld      b, a
+        ld      a, (GroundTarget_Temp_Y_Static)
+        ld      c, a
+
+        push    bc
+                ; check col. between current enemy and shot 0
+                ld      hl, PlayerShot_0_Struct
+                call    CheckCol_GroundTarget_PlayerShot
+        pop     bc
+        push    bc
+                ; check col. between current enemy and shot 1
+                ld      hl, PlayerShot_1_Struct
+                call    CheckCol_GroundTarget_PlayerShot
+        pop     bc
+        push    bc
+                ; check col. between current enemy and shot 2
+                ld      hl, PlayerShot_2_Struct
+                call    CheckCol_GroundTarget_PlayerShot
+        pop     bc
+
 .return:
         ; increment Frame Counter
         ; ld      hl, (GroundTarget_Temp_Frame_Counter)
