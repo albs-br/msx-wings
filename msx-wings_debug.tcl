535a4000ccc12a810fe2931eff16b7fb500bb6f5
Andre Baptista <albs_br@yahoo.com.br>
2022-06-30 22:46:35 -0300

    v.0.41.8

--- msx-wings_debug.tcl
+++ msx-wings_debug.tcl
@@ -6,17 +6,19 @@ ram_watch   add     0xc005      -type byte       -desc Player_Shot_Type      -fo
 
 ram_watch   add     0xC1FD      -type word       -desc LevelCounter      -format dec
 
-ram_watch   add     0xC190      -type byte       -desc GT0_Y_Static      -format dec
+ram_watch   add     0xc3cc      -type byte       -desc LevelInitAnimation_Counter      -format dec
 
-ram_watch   add     0xC201      -type byte       -desc Screen_Y_Origin      -format dec
-ram_watch   add     0xC2cc      -type byte       -desc Output0      -format hex
-ram_watch   add     0xC2cd      -type byte       -desc Output1      -format hex
-ram_watch   add     0xC2ce      -type byte       -desc Output2      -format hex
-ram_watch   add     0xC2cf      -type byte       -desc Output3      -format hex
-ram_watch   add     0xC2cf      -type byte       -desc Output4      -format hex
-ram_watch   add     0xC2d0      -type byte       -desc Output5      -format hex
-ram_watch   add     0xC2d1      -type byte       -desc Output6      -format hex
-ram_watch   add     0xC2d2      -type byte       -desc Output7      -format hex
+#ram_watch   add     0xC190      -type byte       -desc GT0_Y_Static      -format dec
+
+# ram_watch   add     0xC201      -type byte       -desc Screen_Y_Origin      -format dec
+# ram_watch   add     0xC2cc      -type byte       -desc Output0      -format hex
+# ram_watch   add     0xC2cd      -type byte       -desc Output1      -format hex
+# ram_watch   add     0xC2ce      -type byte       -desc Output2      -format hex
+# ram_watch   add     0xC2cf      -type byte       -desc Output3      -format hex
+# ram_watch   add     0xC2cf      -type byte       -desc Output4      -format hex
+# ram_watch   add     0xC2d0      -type byte       -desc Output5      -format hex
+# ram_watch   add     0xC2d1      -type byte       -desc Output6      -format hex
+# ram_watch   add     0xC2d2      -type byte       -desc Output7      -format hex
 
 #GroundTarget_0_Struct: equ 0C18Dh ; last def. pass 3
 #Screen_Y_Origin: equ 0C201h ; last def. pass 3
