7d920f86c61af49a75f51984cef9673f4b99097e
Andre Baptista <albs_br@yahoo.com.br>
2022-07-10 23:05:15 -0300

    v.0.42.0

--- Graphics/Sprites/SpriteAssets.s
+++ Graphics/Sprites/SpriteAssets.s
@@ -20,8 +20,9 @@ PaletteData:
     ; db 0x44, 0x04 ; Color index 15
 
 
-; ------------------------------ Patterns
+; ------------------------------ Patterns  ------------------------------
 
+; Player plane
 ; Center
 SpritePattern_PlayerPlane_0_and_1:
     INCBIN "Graphics/Sprites/PlayerPlane/player_plane_top.pat"
@@ -60,12 +61,14 @@ SpritePattern_PlayerPlane_Right_Frame_1_Bottom:
 
 
 
+; Player shot
 SpritePattern_PlayerShot:
     INCLUDE "Graphics/Sprites/PlayerShot/player_shot_pattern.s"
 .size:  equ $ - SpritePattern_PlayerShot
 
 
 
+; Enemy plane
 SpritePattern_EnemyPlane_Frame_0_Patterns_0_and_1:
     INCBIN "Graphics/Sprites/EnemyPlane/enemy_plane_0.pat"
 .size:  equ $ - SpritePattern_EnemyPlane_Frame_0_Patterns_0_and_1
@@ -92,31 +95,71 @@ SpritePattern_EnemyPlane_Frame_5_Patterns_0_and_1:
 
 
 
+; Enemy plane turning
+SpritePattern_EnemyPlaneTurning_Frame_0_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_0.pat"
+.size:  equ $ - SpritePattern_EnemyPlaneTurning_Frame_0_Patterns_0_and_1
+
+SpritePattern_EnemyPlaneTurning_Frame_1_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_1.pat"
+.size:  equ $ - SpritePattern_EnemyPlaneTurning_Frame_1_Patterns_0_and_1
+
+SpritePattern_EnemyPlaneTurning_Frame_2_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_2.pat"
+.size:  equ $ - SpritePattern_EnemyPlaneTurning_Frame_2_Patterns_0_and_1
+
+SpritePattern_EnemyPlaneTurning_Frame_3_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_3.pat"
+.size:  equ $ - SpritePattern_EnemyPlaneTurning_Frame_3_Patterns_0_and_1
+
+SpritePattern_EnemyPlaneTurning_Frame_4_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_4.pat"
+.size:  equ $ - SpritePattern_EnemyPlaneTurning_Frame_4_Patterns_0_and_1
+
+SpritePattern_EnemyPlaneTurning_Frame_5_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_5.pat"
+.size:  equ $ - SpritePattern_EnemyPlaneTurning_Frame_5_Patterns_0_and_1
+
+SpritePattern_EnemyPlaneTurning_Frame_6_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_6.pat"
+.size:  equ $ - SpritePattern_EnemyPlaneTurning_Frame_6_Patterns_0_and_1
+
+SpritePattern_EnemyPlaneTurning_Frame_7_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_7.pat"
+.size:  equ $ - SpritePattern_EnemyPlaneTurning_Frame_7_Patterns_0_and_1
+
+
+
+; Explosion
 SpritePattern_Explosion_Frames_0_to_2:
     INCLUDE "Graphics/Sprites/Explosion/explosion_frames_patterns.s"
 .size:  equ $ - SpritePattern_Explosion_Frames_0_to_2
 
 
 
+; Enemy shot
 SpritePattern_EnemyShot_0_and_1:
     INCLUDE "Graphics/Sprites/EnemyShot/enemy_shot_patterns.s"
 .size:  equ $ - SpritePattern_EnemyShot_0_and_1
 
 
 
+; Item P
 SpritePattern_Item_P_Frames_0_to_7:
     INCLUDE "Graphics/Sprites/Item_P/item_p_patterns.s"
 .size:  equ $ - SpritePattern_Item_P_Frames_0_to_7
 
 
 
+; Ground target
 SpritePattern_GroundTarget_0:
     INCLUDE "Graphics/Sprites/GroundTarget/ground_target_pattern_0.s"
 .size:  equ $ - SpritePattern_GroundTarget_0
 
 
-; ------------------------------ Colors
+; ------------------------------ Colors  ------------------------------
 
+; Player plane
 ; Center
 SpriteColors_PlayerPlane_0_and_1:
     INCBIN "Graphics/Sprites/PlayerPlane/player_plane_top.col"
@@ -156,12 +199,14 @@ SpriteColors_PlayerPlane_Right_Frame_1_Bottom:
 
 
 
+; Player shot
 SpriteColors_PlayerShot:
     INCLUDE "Graphics/Sprites/PlayerShot/player_shot_color.s"
 .size:  equ $ - SpriteColors_PlayerShot
 
 
 
+; Enemy plane
 SpriteColors_EnemyPlane_Frame_0_Patterns_0_and_1:
     INCBIN "Graphics/Sprites/EnemyPlane/enemy_plane_0.col"
 .size:  equ $ - SpriteColors_EnemyPlane_Frame_0_Patterns_0_and_1
@@ -188,24 +233,63 @@ SpriteColors_EnemyPlane_Frame_5_Patterns_0_and_1:
 
 
 
+; Enemy plane turning
+SpriteColors_EnemyPlaneTurning_Frame_0_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_0.col"
+.size:  equ $ - SpriteColors_EnemyPlaneTurning_Frame_0_Patterns_0_and_1
+
+SpriteColors_EnemyPlaneTurning_Frame_1_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_1.col"
+.size:  equ $ - SpriteColors_EnemyPlaneTurning_Frame_1_Patterns_0_and_1
+
+SpriteColors_EnemyPlaneTurning_Frame_2_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_2.col"
+.size:  equ $ - SpriteColors_EnemyPlaneTurning_Frame_2_Patterns_0_and_1
+
+SpriteColors_EnemyPlaneTurning_Frame_3_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_3.col"
+.size:  equ $ - SpriteColors_EnemyPlaneTurning_Frame_3_Patterns_0_and_1
+
+SpriteColors_EnemyPlaneTurning_Frame_4_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_4.col"
+.size:  equ $ - SpriteColors_EnemyPlaneTurning_Frame_4_Patterns_0_and_1
+
+SpriteColors_EnemyPlaneTurning_Frame_5_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_5.col"
+.size:  equ $ - SpriteColors_EnemyPlaneTurning_Frame_5_Patterns_0_and_1
+
+SpriteColors_EnemyPlaneTurning_Frame_6_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_6.col"
+.size:  equ $ - SpriteColors_EnemyPlaneTurning_Frame_6_Patterns_0_and_1
+
+SpriteColors_EnemyPlaneTurning_Frame_7_Patterns_0_and_1:
+    INCBIN "Graphics/Sprites/EnemyPlaneTurning/enemy_plane_turning_7.col"
+.size:  equ $ - SpriteColors_EnemyPlaneTurning_Frame_7_Patterns_0_and_1
+
+
+
+; Explosion
 SpriteColors_Explosion_Frames_0_to_2:
     INCLUDE "Graphics/Sprites/Explosion/explosion_frames_colors.s"
 .size:  equ $ - SpriteColors_Explosion_Frames_0_to_2
 
 
 
+; Enemy shot
 SpriteColors_EnemyShot_0_to_2:
     INCLUDE "Graphics/Sprites/EnemyShot/enemy_shot_colors.s"
 .size:  equ $ - SpriteColors_EnemyShot_0_to_2
 
 
 
+; Item P
 SpriteColors_Item_P_Frame_0:
     INCLUDE "Graphics/Sprites/Item_P/item_p_colors.s"
 .size:  equ $ - SpriteColors_Item_P_Frame_0
 
 
 
+; Ground target
 SpriteColors_GroundTarget_0:
     INCLUDE "Graphics/Sprites/GroundTarget/ground_target_colors_0.s"
 .size:  equ $ - SpriteColors_GroundTarget_0
