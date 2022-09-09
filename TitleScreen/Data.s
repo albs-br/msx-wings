0219ae097b3426e2bb6b98c73364274ed0494c75
Andre Baptista <albs_br@yahoo.com.br>
2022-09-09 18:51:13 -0300

    v.0.45.6

--- TitleScreen/Data.s
+++ TitleScreen/Data.s
@@ -48,49 +48,49 @@ TitleColor_1_End:
 
 
 ; ------------------ b & w palette
-; Title_PaletteData:
-;     ;  data 1 (red 0-7; blue 0-7); data 2 (0000; green 0-7)
-;     db 0x77, 0x07 ; Color index 0
-; Title_PaletteData_1:
-;     db 0x00, 0x00 ; Color index 1
-;     db 0x11, 0x01 ; Color index 2
-;     db 0x11, 0x01 ; Color index 3
-;     db 0x22, 0x02 ; Color index 4
-;     db 0x22, 0x02 ; Color index 5
-;     db 0x33, 0x03 ; Color index 6
-;     db 0x33, 0x03 ; Color index 7
-;     db 0x44, 0x04 ; Color index 8
-;     db 0x44, 0x04 ; Color index 9
-;     db 0x55, 0x05 ; Color index 10 (0xa)
-;     db 0x55, 0x05 ; Color index 11 (0xb)
-;     db 0x66, 0x06 ; Color index 12 (0xc)
-;     db 0x66, 0x06 ; Color index 13 (0xd)
-;     db 0x77, 0x07 ; Color index 14 (0xe)
-; Title_PaletteData_End_1:
-;     db 0x00, 0x00 ; Color index 15 (0xf)
-
-; ------------------ alternative palette
 Title_PaletteData:
     ;  data 1 (red 0-7; blue 0-7); data 2 (0000; green 0-7)
     db 0x77, 0x07 ; Color index 0
 Title_PaletteData_1:
     db 0x00, 0x00 ; Color index 1
-
-    ; shades of red:
-    db 0x10, 0x00 ; Color index 2
-    db 0x20, 0x00 ; Color index 3
-    db 0x30, 0x00 ; Color index 4
-    db 0x40, 0x00 ; Color index 5
-    db 0x50, 0x00 ; Color index 6
-    db 0x60, 0x00 ; Color index 7
-    db 0x70, 0x00 ; Color index 8
-
-    ; shades of gray:
-    db 0x11, 0x01 ; Color index 9
-    db 0x22, 0x02 ; Color index 10 (0xa)
-    db 0x33, 0x03 ; Color index 11 (0xb)
-    db 0x44, 0x04 ; Color index 12 (0xc)
-    db 0x55, 0x05 ; Color index 13 (0xd)
-    db 0x66, 0x06 ; Color index 14 (0xe)
+    db 0x11, 0x01 ; Color index 2
+    db 0x11, 0x01 ; Color index 3
+    db 0x22, 0x02 ; Color index 4
+    db 0x22, 0x02 ; Color index 5
+    db 0x33, 0x03 ; Color index 6
+    db 0x33, 0x03 ; Color index 7
+    db 0x44, 0x04 ; Color index 8
+    db 0x44, 0x04 ; Color index 9
+    db 0x55, 0x05 ; Color index 10 (0xa)
+    db 0x55, 0x05 ; Color index 11 (0xb)
+    db 0x66, 0x06 ; Color index 12 (0xc)
+    db 0x66, 0x06 ; Color index 13 (0xd)
+    db 0x77, 0x07 ; Color index 14 (0xe)
 Title_PaletteData_End_1:
     db 0x00, 0x00 ; Color index 15 (0xf)
+
+; ; ------------------ alternative palette
+; Title_PaletteData:
+;     ;  data 1 (red 0-7; blue 0-7); data 2 (0000; green 0-7)
+;     db 0x77, 0x07 ; Color index 0
+; Title_PaletteData_1:
+;     db 0x00, 0x00 ; Color index 1
+
+;     ; shades of red:
+;     db 0x10, 0x00 ; Color index 2
+;     db 0x20, 0x00 ; Color index 3
+;     db 0x30, 0x00 ; Color index 4
+;     db 0x40, 0x00 ; Color index 5
+;     db 0x50, 0x00 ; Color index 6
+;     db 0x60, 0x00 ; Color index 7
+;     db 0x70, 0x00 ; Color index 8
+
+;     ; shades of gray:
+;     db 0x11, 0x01 ; Color index 9
+;     db 0x22, 0x02 ; Color index 10 (0xa)
+;     db 0x33, 0x03 ; Color index 11 (0xb)
+;     db 0x44, 0x04 ; Color index 12 (0xc)
+;     db 0x55, 0x05 ; Color index 13 (0xd)
+;     db 0x66, 0x06 ; Color index 14 (0xe)
+; Title_PaletteData_End_1:
+;     db 0x00, 0x00 ; Color index 15 (0xf)
