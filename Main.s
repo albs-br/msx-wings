0f444badb18dd41c9bb7f91255d33980481fa661
Andre Baptista <albs_br@yahoo.com.br>
2022-10-20 22:10:30 -0300

    v.0.50.0

--- Main.s
+++ Main.s
@@ -135,7 +135,7 @@ Execute:
 
 
 
-    ld      a, 1                        ; level number (1-8)
+    ld      a, 4                        ; level number (1-8)
     ld      (CurrentLevelNumber), a
     call    LoadLevel
 
